d4b5fc0bc35ea646eed6ef2e3b55700fadce6f48
njoung <njoung@yahoo.com>
2014-12-10 17:53:00 +0000

    password resets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,10 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = false
+  host = 'whispering-beyond-9103.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true

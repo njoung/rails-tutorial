bc5895a62c42b10b421571a73dd8dcefeef40095
njoung <njoung@yahoo.com>
2014-12-10 22:07:01 +0000

    3

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,22 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Rails.application.configure do
+  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  host = '<your heroku app>.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
